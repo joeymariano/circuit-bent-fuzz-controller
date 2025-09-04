fe657c06c63f943231c3eaa0314a39a6840f1f48
joeymariano <joey.michalina.mariano@gmail.com>
2025-09-03 23:10:32 -0400

    toggles main led whenever mode is hit

--- circuit-bent-fuzz.ino
+++ circuit-bent-fuzz.ino
@@ -6,37 +6,37 @@
 // 2. momentary tremolo
 // 3. latching
 // 4. latching tremelo
-// two buttons and two transistor switches
-
-// idea => hook main LED to arduino and use it to blink the mode
-// use power from LED to power the arduino?
-// blink once for mode 1 twice for mode 2 etc.
+// two buttons and 3 transistor switches
 
 // 2N7000 transistor switches
-int transistorSwitches[] = {5, 6, 2};
+int transistorSwitches[] = { 5, 6 };
+
 // 2 is for a switch to interupt the pedal's main led
+int modeIndicator = 2;
 
 // buttons
 int modeButton = 10;
-int buttons[] = {3, 4};
+int buttons[] = { 3, 4 };
 
 // default mode for switching
 int mode = 1;
 
-int millisReading[] = {0, 0};
-int lastToggleTime[] = {0, };
-int tremDelay[] = {60, 60};
-bool tremFlag[] = {false, false};
+int millisReading[] = { 0, 0 };
+int lastToggleTime[] = {
+  0,
+};
+int tremDelay[] = { 60, 60 };
+bool tremFlag[] = { false, false };
 
 // extra variables for latching functions
 const int NUM_BUTTONS = 2;
-int buttonPins[NUM_BUTTONS] = {buttons[0], buttons[1]};
+int buttonPins[NUM_BUTTONS] = { buttons[0], buttons[1] };
 int reading[NUM_BUTTONS];
 int lastButtonState[NUM_BUTTONS];
 unsigned long lastDebounceTime[NUM_BUTTONS];
 int buttonState[NUM_BUTTONS];
 bool buttonPressed[NUM_BUTTONS];
-int transistorState[NUM_BUTTONS];         // If each button controls a separate LED
+int transistorState[NUM_BUTTONS];  // If each button controls a separate LED
 unsigned long debounceDelay = 50;  // 50 milliseconds debounce time
 
 // more for latching tremolo
@@ -49,23 +49,28 @@ void setup() {
   Serial.println("serial succes");
 
   for (int i = 0; i < 2; i++) {
+    // setup buttons as inputs
     pinMode(buttons[i], INPUT_PULLUP);
+    // setup transistor switches as outputs
     pinMode(transistorSwitches[i], OUTPUT);
   }
-  digitalWrite(transistorSwitches[2], HIGH);
+  // set mode button as input
   pinMode(modeButton, INPUT_PULLUP);
+  // set up a switch to control the main led ~make it blink
+  pinMode(modeIndicator, OUTPUT);
+
+  digitalWrite(modeIndicator, HIGH);  // oopen the main led switch
 }
 
 void loop() {
   switch (mode) {
     case 1:
-      // need to break this up like latching case
+      // blinkMain(); if first loop
       momentary(0);
       momentary(1);
       break;
 
     case 2:
-      // need to break this up like latching case
       momentaryTrem(0);
       momentaryTrem(1);
       break;
@@ -90,7 +95,8 @@ void setMode() {
   static unsigned long lastDebounceTime = 0;
   static int lastButtonState = HIGH;
   static int modeButtonState = HIGH;
-  const unsigned long debounceDelay = 50; // ms
+  const unsigned long debounceDelay = 50;  // ms
+  static int previousMode = -1;            // Track last mode for change detection
 
   int reading = digitalRead(modeButton);
 
@@ -101,19 +107,31 @@ void setMode() {
 
   // if the button is LOW and it's been LOW for long enough, change mode
   if ((reading == LOW) && (millis() - lastDebounceTime > debounceDelay)) {
-    if (modeButtonState == HIGH) { // only toggle once per button press
-      if (mode >= 4){
+    // only toggle once per button press
+    if (modeButtonState == HIGH) {
+      if (mode >= 4) {
         mode = 0;
       }
       mode++;
       modeButtonState = LOW;
+
+      // call blinkMain if mode changed
+      if (mode != previousMode) {
+        blinkMain();
+        previousMode = mode;
+      }
     }
   }
 
   // reset when button is released
   if (reading == HIGH) {
+    // animation effect goes here
     modeButtonState = HIGH;
   }
 
   lastButtonState = reading;
+}
+
+void blinkMain() {
+  digitalWrite(modeIndicator, !digitalRead(modeIndicator));  // toggle
 }
